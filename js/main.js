7b2dd69b16c6d4d343a61798663d91e0cd55fce8
JoshMuscat <47892683+JoshMuscat@users.noreply.github.com>
2020-07-16 23:06:45 +1000

    Overhaul 2020

--- js/main.js
+++ js/main.js
@@ -520,8 +520,7 @@ jQuery(document).ready(function(){
     });
 
 jQuery(document).ready(function(){
-        jQuery('#hideshow2').on('click', function(event) { 
-            $("img.hiddenload").reveal("fadeIn", 200);       
+        jQuery('#hideshow2').on('click', function(event) {        
             jQuery('#content2').slideToggle(800);
              if($('#hideshow2').val() == '+') {
                 jQuery('#hideshow2').val("-");
@@ -544,28 +543,12 @@ jQuery(document).ready(function(){
         });
     });
 
-(function($){
-    // Bind the function to global jQuery object.
-    $.fn.reveal = function(){
-        // Arguments is a variable which is available within all functions
-        // and returns an object which holds the arguments passed.
-        // It is not really an array, so by calling Array.prototype
-        // he is actually casting it into an array.
-        var args = Array.prototype.slice.call(arguments);
+jQuery(document).ready(function($) {
+    $(".mainfx").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function(){
+    $(this).removeClass("animatedspin")  
+    })
 
-        // For each elements that matches the selector:
-        return this.each(function(){
-            // this is the dom element, so encapsulate the element with jQuery.
-            var img = $(this),
-                src = img.data("src");
-
-            // If there is a data-src attribute, set the attribute
-            // src to make load the image and bind an onload event.
-            src && img.attr("src", src).load(function(){
-                // Call the first argument passed (like fadeIn, slideIn, default is 'show').
-                // This piece is like doing img.fadeIn(1000) but in a more dynamic way.
-                img[args[0]||"show"].apply(img, args.splice(1));
-            });
-        });
-    }
-}(jQuery));
+    $(".mainfx").hover(function(){
+    $(this).addClass("animatedspin");        
+    })
+});
